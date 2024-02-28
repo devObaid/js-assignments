edfb8ebcd6162b30b30075f0ee07d94005ab858c
devObaid <obaidshahid2001@gmail.com>
2024-02-28 00:23:42 -1200

    2nd assignment done

--- assignment2/app.js
+++ assignment2/app.js
@@ -1,2 +1,22 @@
 var userName;
-var fullName = 'Muhammad Obaid'
+var fullName = 'Muhammad Obaid';
+var message = 'hello world';
+var myAge = '18';
+alert(message);
+alert( 'my name is ' + fullName);
+alert( 'I am ' + myAge + '  years old');
+var pizza = 'PIZZA';
+var pizz = 'PIZZ';
+var piz = 'PIZ';
+var pi = 'PI';
+var p = 'P';
+alert( pizza +'\n'+pizz+'\n'+piz+'\n'+pi+'\n'+p);
+var email = 'example@gmail.com';
+alert('My email is ' + email);
+var book = 'smarter way to learn js ';
+alert('I am trying to learn from a book called' + book);
+var design = '“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+alert(design)
+
+
+
